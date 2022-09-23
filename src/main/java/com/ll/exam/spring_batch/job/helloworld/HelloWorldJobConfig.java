1bf23ef0ff51dfcd0bdb06bc1a9ed0a078353fe3
KCH <doolysmile1@naver.com>
2022-09-23 13:41:41 +0900

    feat : 배치 스탭 추가

--- src/main/java/com/ll/exam/spring_batch/job/helloworld/HelloWorldJobConfig.java
+++ src/main/java/com/ll/exam/spring_batch/job/helloworld/HelloWorldJobConfig.java
@@ -3,13 +3,11 @@ package com.ll.exam.spring_batch.job.helloworld;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -18,39 +16,56 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @RequiredArgsConstructor
 public class HelloWorldJobConfig {
-
-
     private final JobBuilderFactory jobBuilderFactory;
-
 
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
     public Job helloWorldJob() {
         return jobBuilderFactory.get("helloWorldJob")
-                .incrementer(new RunIdIncrementer())
+                //.incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID를 실행시에 파라미터로 부여
                 .start(helloWorldStep1())
+                .next(helloWorldStep2())
                 .build();
     }
 
-    @JobScope
     @Bean
+    @JobScope
     public Step helloWorldStep1() {
         return stepBuilderFactory.get("helloWorldStep1")
-                .tasklet(helloWorldTasklet())
+                .tasklet(helloWorldStep1Tasklet())
                 .build();
     }
 
-    @StepScope
     @Bean
-    public Tasklet helloWorldTasklet() {
-        return new Tasklet() {
-            @Override
-            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                System.out.println("헬로 월드");
-                return RepeatStatus.FINISHED;
-            }
+    @StepScope
+    public Tasklet helloWorldStep1Tasklet() {
+        return (contribution, chunkContext) -> {
+            System.out.println("헬로월드 테스클릿 1");
+
+            return RepeatStatus.FINISHED;
         };
     }
-    
+
+    @Bean
+    @JobScope
+    public Step helloWorldStep2() {
+        return stepBuilderFactory.get("helloWorldStep2")
+                .tasklet(helloWorldStep2Tasklet())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet helloWorldStep2Tasklet() {
+        return (contribution, chunkContext) -> {
+            System.out.println("헬로월드 테스클릿 2");
+
+            if ( false ) {
+                throw new Exception("실패 : 헬로월드 테스클릿 2");
+            }
+
+            return RepeatStatus.FINISHED;
+        };
+    }
 }
